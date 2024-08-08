561b3ec6e575f5061129c393b08a5dbc03c3a91e
mauricekoenig <60366053+mauricekoenig@users.noreply.github.com>
2024-08-08 22:18:53 +0200

    Create CardViewLayoutSettings.cs

---
+++ CardViewLayoutSettings.cs
@@ -0,0 +1,20 @@
+
+
+using UnityEngine;
+
+[CreateAssetMenu]
+public class CardViewLayoutSettings : ScriptableObject {
+
+
+    public int MaxHandSize;
+    public int MaxBoardSize;
+
+    public float AnimationSpeed;
+
+    public float HandWidth;
+    public float BaseOffsetY;
+    public float Depth;
+
+    public float OverlapHand;
+    public float OverlapBoard;
+}
