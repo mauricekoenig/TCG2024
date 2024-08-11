d06ca8b9d439a2e89eb32a0c4041a294004c2eff
mauricekoenig <60366053+mauricekoenig@users.noreply.github.com>
2024-08-11 22:27:05 +0200

    Create CardData.cs

---
+++ CardData.cs
@@ -0,0 +1,13 @@
+
+
+using UnityEngine;
+
+[CreateAssetMenu]
+public class CardData : ScriptableObject {
+
+    public string Name;
+    public int Cost;
+    public int HP;
+    public int Attack;
+    public int Defense;
+}
