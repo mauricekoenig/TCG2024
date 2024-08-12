ca447ade1693d581b3763fac6f4eef29a4ff89d0
mauricekoenig <60366053+mauricekoenig@users.noreply.github.com>
2024-08-12 17:25:18 +0200

    Update Player.cs

--- Player.cs
+++ Player.cs
@@ -1,19 +1,37 @@
 
 
 
+using Mk.Tcg.Core;
+using NUnit.Framework;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Mk.Tcg.Gameplay {
+
+    [RequireComponent(typeof(PlayerCards))]
+    [RequireComponent(typeof(PlayerResources))]
 
     public abstract class Player : MonoBehaviour {
 
         [SerializeField] protected int id;
         [SerializeField] protected string playerName;
+        [SerializeField] protected PlayerCards cards;
+        [SerializeField] protected PlayerResources resources;
 
-        public void Init (int id, string name) {
+        public void Init (int id, string name, List<CardData> deck, int startingResources) {
+
+
+            cards = GetComponent<PlayerCards>();
+            resources = GetComponent<PlayerResources>();
 
             this.id = id;
             playerName = name;
+            cards.Init(deck);
+            resources.SetCurrentResources(startingResources);
+        }
+
+        public void SetObjectName (string name) {
+            gameObject.name = name;
         }
     }
 }
