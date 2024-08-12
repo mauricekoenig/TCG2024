b4ccc5abcb785e623f00ab5ab16db87f1c3359b2
mauricekoenig <60366053+mauricekoenig@users.noreply.github.com>
2024-08-12 17:28:10 +0200

    Update TurnController.cs

--- TurnController.cs
+++ TurnController.cs
@@ -1,3 +1,4 @@
+using Mk.Tcg.Core;
 using System;
 using System.Collections.Generic;
 using UnityEngine;
@@ -12,44 +13,12 @@ namespace Mk.Tcg.Gameplay {
         [SerializeField] private GameObject humanPlayerPrefab;
         private Queue<Player> players = new();
 
-
         public event Action <TurnState> OnStateChanged;
         public event Action <Player> OnActivePlayerChanged;
         public event Action <Player> OnDrawEnter;
         public event Action <Player> OnMainEnter;
         public event Action <Player> OnBattleEnter;
         public event Action <Player> OnEndEnter;
-
-        private void Start() {
-
-            RegisterPlayer(CreatePlayer(1, "Maurice"));
-            RegisterPlayer(CreatePlayer(2, "Demon"));
-            activePlayer = players.Peek();
-
-            SetState(TurnState.Draw);
-        }
-
-        private Player CreatePlayer (int id, string name) {
-
-            Player player = Instantiate(humanPlayerPrefab).GetComponent<Player>();
-            player.Init(id, name);
-            player.gameObject.name = $"Player: {name}";
-            return player;
-        }
-
-        public void RegisterPlayer (Player player) {
-
-            players.Enqueue(player);
-        }
-
-        private void NextPlayer () {
-
-            if (players.Count == 0) return;
-            Player player = players.Dequeue();
-            players.Enqueue(player);
-            activePlayer = players.Peek();
-            OnActivePlayerChanged?.Invoke(activePlayer);
-        }
 
         public void SetState (TurnState state) {
 
@@ -59,6 +28,7 @@ namespace Mk.Tcg.Gameplay {
                     break;
 
                 case TurnState.Draw:
+                    currentState = TurnState.Draw;
                     OnDrawEnter?.Invoke(activePlayer);
                     break;
 
@@ -66,6 +36,31 @@ namespace Mk.Tcg.Gameplay {
                     OnMainEnter?.Invoke(activePlayer);
                     break;
             }
+        }
+
+        public Player CreatePlayer (int id, string name, List<CardData> deck, int startingResources) {
+
+            Player player = Instantiate(humanPlayerPrefab).GetComponent<Player>();
+
+            player.Init(id, name, deck, startingResources);
+            player.SetObjectName($"Player: {name}");
+
+            return player;
+        }
+
+        public void RegisterPlayer (Player player) {
+
+            players.Enqueue(player);
+            activePlayer = players.Peek();
+        }
+
+        private void NextPlayer () {
+
+            if (players.Count == 0) return;
+            Player player = players.Dequeue();
+            players.Enqueue(player);
+            activePlayer = players.Peek();
+            OnActivePlayerChanged?.Invoke(activePlayer);
         }
 
     }
