37dc8ddfb56213d08d46170c006842757f3a8915
mauricekoenig <60366053+mauricekoenig@users.noreply.github.com>
2024-08-12 17:27:31 +0200

    Create Util.cs

---
+++ Util.cs
@@ -0,0 +1,13 @@
+
+
+using UnityEngine;
+
+namespace Mk.Tcg.Core {
+
+    public static class Util {
+
+        public static Vector3 Vec (float x, float y, float z) {
+            return new Vector3(x, y, z);
+        }
+    }
+}
